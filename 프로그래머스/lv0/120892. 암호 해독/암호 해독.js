db5f511d23bdae269364537abfc9b78796dc24a8
heereal <117061017+heereal@users.noreply.github.com>
2023-01-02 19:04:45 +0900

    [level 0] Title: 암호 해독, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120892. 암호 해독/암호 해독.js
+++ 프로그래머스/lv0/120892. 암호 해독/암호 해독.js
@@ -1,8 +1,7 @@
 function solution(cipher, code) {
-    let array = [...cipher];
     let answer = ''
-    for (i=code-1; i<array.length; i+=code) {
-        answer += array[i]
+    for (i=code-1; i<cipher.length; i+=code) {
+        answer += cipher[i]
     }
     return answer  
 }
