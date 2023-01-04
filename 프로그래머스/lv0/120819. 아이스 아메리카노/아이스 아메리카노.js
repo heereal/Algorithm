3a9f673a5db3d0140527c81d7dd70433b63ae995
heereal <117061017+heereal@users.noreply.github.com>
2023-01-04 20:42:24 +0900

    [level 0] Title: 아이스 아메리카노, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120819. 아이스 아메리카노/아이스 아메리카노.js
+++ 프로그래머스/lv0/120819. 아이스 아메리카노/아이스 아메리카노.js
@@ -1,6 +1,3 @@
 function solution(money) {
-    let answer = [];
-    answer.push(parseInt(money / 5500))
-    answer.push(money % 5500)
-    return answer
+    return [parseInt(money / 5500), money % 5500]
 }
