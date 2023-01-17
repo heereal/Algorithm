297c88dbdaf4e46877d6ae71c6e2eceb25e2c14b
이희령 <117061017+heereal@users.noreply.github.com>
2023-01-17 20:47:14 +0900

    [level 0] Title: 최댓값 만들기 (1), Time: 0.05 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.js
+++ 프로그래머스/lv0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.js
@@ -1,11 +1,4 @@
 function solution(numbers) {
-    let answer = 0;
-    for (const i in numbers) {
-        for (const i2 in numbers) {
-            if ( i !== i2 && numbers[i] * numbers[i2] >= answer) {
-                answer = numbers[i] * numbers[i2]
-            }
-        }
-    }
-    return answer
+    numbers.sort((a, b) => b - a);
+    return numbers[0]*numbers[1]
 }
