2b852a22b0305fdf3d0d7404e8e244489c25fa1b
이희령 <117061017+heereal@users.noreply.github.com>
2023-01-19 16:07:28 +0900

    [level 0] Title: 외계행성의 나이, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120834. 외계행성의 나이/외계행성의 나이.js
+++ 프로그래머스/lv0/120834. 외계행성의 나이/외계행성의 나이.js
@@ -1,4 +1,3 @@
 function solution(age) {
-    const arr = Array.from({ length: 26 }, (v, i) => String.fromCharCode(i + 97));
-    return (age + "").split("").map((n) => arr[n]).join("")
+    return (age + "").split("").map((n) => "abcdefghij"[n]).join("")
 }
