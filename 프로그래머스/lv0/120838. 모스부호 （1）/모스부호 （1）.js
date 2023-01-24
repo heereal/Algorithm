8b95daa48ccc3eec073a502b5c3842d224f5dc5d
이희령 <117061017+heereal@users.noreply.github.com>
2023-01-24 19:40:52 +0900

    [level 0] Title: 모스부호 (1), Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120838. 모스부호 （1）/모스부호 （1）.js
+++ 프로그래머스/lv0/120838. 모스부호 （1）/모스부호 （1）.js
@@ -1,11 +1,11 @@
-function solution(letter) {
-    morse = { 
+let morse = { 
     '.-':'a','-...':'b','-.-.':'c','-..':'d','.':'e','..-.':'f',
     '--.':'g','....':'h','..':'i','.---':'j','-.-':'k','.-..':'l',
     '--':'m','-.':'n','---':'o','.--.':'p','--.-':'q','.-.':'r',
     '...':'s','-':'t','..-':'u','...-':'v','.--':'w','-..-':'x',
     '-.--':'y','--..':'z'
-    }
-    
-    return letter.split(' ').map((v) => Object.keys(morse).includes(v) ? morse[v] : null).join("")
+}
+
+function solution(letter) {
+    return letter.split(' ').reduce((acc, cur) => acc + morse[cur], '')
 }
