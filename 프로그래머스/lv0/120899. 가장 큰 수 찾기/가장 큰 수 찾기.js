bb6e2fdb1a6ae39a9f13fe52b0db878c21e1db78
이희령 <117061017+heereal@users.noreply.github.com>
2023-02-02 10:00:51 +0900

    [level 0] Title: 가장 큰 수 찾기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.js
+++ 프로그래머스/lv0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.js
@@ -1,11 +1,4 @@
 function solution(array) {
-    let max_num = array[0];
-    let answer = [];
-    for (const i in array) {
-      if (array[i] > max_num) {
-          max_num = array[i];
-          answer = [max_num, +i];
-      }
-    }
-    return answer;
+    let max_num = Math.max(...array);
+    return [max_num, array.indexOf(max_num)]
 }
