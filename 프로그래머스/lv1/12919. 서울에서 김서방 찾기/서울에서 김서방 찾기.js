3ad9c8bf09f8682ef83d484f70d7e7c348b71c32
이희령 <117061017+heereal@users.noreply.github.com>
2023-03-16 22:41:36 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 0.03 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.js
+++ 프로그래머스/lv1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.js
@@ -1,5 +1,3 @@
 function solution(seoul) {
-    const array = [];
-    seoul.map((string, index) => string === 'Kim' && array.push(index))
-    return `김서방은 ${array[0]}에 있다`;
+    return `김서방은 ${seoul.indexOf("Kim")}에 있다`;
 }
