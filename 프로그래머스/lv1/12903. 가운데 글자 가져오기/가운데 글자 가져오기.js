f5de3d79062ab3e8665f7fa22416f109ac051e4a
이희령 <117061017+heereal@users.noreply.github.com>
2023-03-18 14:49:56 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.js
+++ 프로그래머스/lv1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.js
@@ -1,4 +1,4 @@
 function solution(s) {
     let mid = s.length / 2;
-    return s.length % 2 === 0 ? s[mid-1] + s[mid] : s[Math.floor(mid)];
+    return s.length % 2 === 0 ? s.substring(mid-1, mid+1) : s.substring(mid,mid+1);
 }
