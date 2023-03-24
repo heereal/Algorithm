7166f89b62e1d525fa586333695c5919dca8b1b0
이희령 <117061017+heereal@users.noreply.github.com>
2023-03-24 09:11:33 +0900

    [level 1] Title: 문자열 내림차순으로 배치하기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12917. 문자열 내림차순으로 배치하기/문자열 내림차순으로 배치하기.js
+++ 프로그래머스/lv1/12917. 문자열 내림차순으로 배치하기/문자열 내림차순으로 배치하기.js
@@ -1,6 +1,3 @@
 function solution(s) {
-    let upperCase = [];
-    let lowerCase = [];
-    s.split("").forEach((str) => str !== str.toUpperCase() ? lowerCase.push(str) : upperCase.push(str))
-    return lowerCase.sort().reverse().join("") + upperCase.sort().reverse().join("");
+    return s.split("").sort().reverse().join("")
 }
