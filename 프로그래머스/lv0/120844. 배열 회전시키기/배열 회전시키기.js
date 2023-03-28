6b2c3ddf6c5df763bd749e8cbe455c7c7aa9fab9
이희령 <117061017+heereal@users.noreply.github.com>
2023-03-29 06:46:10 +0900

    [level 0] Title: 배열 회전시키기, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120844. 배열 회전시키기/배열 회전시키기.js
+++ 프로그래머스/lv0/120844. 배열 회전시키기/배열 회전시키기.js
@@ -1,12 +1,4 @@
 function solution(numbers, direction) {
-    if ( direction === "right" ) {
-        let right = numbers[numbers.length - 1]
-        numbers.splice(numbers.length - 1, 1);
-        numbers.unshift(right)
-    } else {
-        let left = numbers[0]
-        numbers.splice(0, 1);
-        numbers.push(left)
-    }
+    direction === "right" ? numbers.unshift(numbers.pop()) : numbers.push(numbers.shift())
     return numbers;
 }
