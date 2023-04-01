424f8a40240cc8724b076bc142d15c8413d13a8d
이희령 <117061017+heereal@users.noreply.github.com>
2023-04-01 21:52:21 +0900

    [level 0] Title: 한 번만 등장한 문자, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120896. 한 번만 등장한 문자/README.md
+++ 프로그래머스/lv0/120896. 한 번만 등장한 문자/README.md
@@ -1,10 +1,10 @@
 # [level 0] 한 번만 등장한 문자 - 120896 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120896?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120896) 
 
 ### 성능 요약
 
-메모리: 33.4 MB, 시간: 0.09 ms
+메모리: 33.4 MB, 시간: 0.05 ms
 
 ### 구분
 
--- 프로그래머스/lv0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.js
+++ 프로그래머스/lv0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.js
@@ -1,9 +1,5 @@
 function solution(s) {
-    let array = [...new Set(s.split("").sort())];
-    let answer = "";
-    for ( i = 0; i <= array.length - 1; i++ ) {
-        let array2 = s.split("").filter((v) => v === array[i]);
-        if (array2.length === 1) answer += array[i];
-    }
-    return answer;
+    let answer = [];
+    for (const str of s) if ( s.indexOf(str) === s.lastIndexOf(str)) answer.push(str);
+    return answer.sort().join("");
 }
