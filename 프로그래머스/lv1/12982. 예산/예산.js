04c9cde16493478fdb6aed027442caf8a31c1197
이희령 <117061017+heereal@users.noreply.github.com>
2023-04-23 15:12:27 +0900

    [level 1] Title: 예산, Time: 0.14 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12982. 예산/예산.js
+++ 프로그래머스/lv1/12982. 예산/예산.js
@@ -1,11 +1,11 @@
 function solution(d, budget) {
     let sum = 0;
+    let answer = 0;
     d.sort((a,b) => a - b);
-    // if (d.length === 1) return 1;
-    for (const index in d) {
-        sum += d[index];
-        if (sum === budget) return parseInt(index) + 1;
-        if (sum > budget) return parseInt(index);
-        if (parseInt(index) === d.length - 1) return parseInt(index) + 1;
+    for (const num of d) {
+        sum += num;
+        if (sum > budget) break;
+        answer++;
     }
+    return answer;
 }
