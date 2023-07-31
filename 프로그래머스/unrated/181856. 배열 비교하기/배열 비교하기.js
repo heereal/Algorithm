980c2b767b90220b383109fdd1d19d3cf62955a3
이희령 <winterbooks@naver.com>
2023-07-31 14:01:16 +0900

    [unrated] Title: 배열 비교하기, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181856. 배열 비교하기/배열 비교하기.js
@@ -0,0 +1,8 @@
+function solution(arr1, arr2) {
+    if (arr1.length === arr2.length) {
+        const sum1 = arr1.reduce((acc, cur) => acc + cur, 0);
+        const sum2 = arr2.reduce((acc, cur) => acc + cur, 0);
+        return sum1 === sum2 ? 0 : sum1 > sum2 ? 1 : -1;
+    }
+    else return arr1.length > arr2.length ? 1 : -1;
+}
