59f4b6fb85c1afb0aad4482f9e55005c1b5ae051
이희령 <winterbooks@naver.com>
2023-08-27 18:10:01 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.06 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.js
+++ 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.js
@@ -1,17 +1,7 @@
 function solution(s) {
     let arr = s.split(" ");
     for (let i = 0; i < arr.length; i++) {
-        let arr2 = arr[i].split("");
-        for (let j = 0; j < arr2.length; j++) {
-            const regex = /[1234567890]/;
-            if (regex.test(arr[i][j])) continue;
-            if (j === 0) {
-                arr2[j] = arr2[j].toUpperCase();
-                continue;
-            };
-            if (j !== 0) arr2[j] = arr2[j].toLowerCase();      
-            }
-        arr[i] = arr2.join("");
-        }
+        arr[i] = arr[i].charAt(0).toUpperCase() + arr[i].substring(1).toLowerCase()
+    }
     return arr.join(" ");
 }
