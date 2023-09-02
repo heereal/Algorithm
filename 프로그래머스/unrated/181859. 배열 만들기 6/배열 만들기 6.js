33a47b892033be2afe1095a1857a078a44fcefea
이희령 <winterbooks@naver.com>
2023-09-02 21:17:19 +0900

    [unrated] Title: 배열 만들기 6, Time: 5.08 ms, Memory: 37.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181859. 배열 만들기 6/배열 만들기 6.js
@@ -0,0 +1,15 @@
+function solution(arr) {
+    let i = 0;
+    let stk = [];
+    while (i < arr.length) {
+        if (stk.length === 0) {
+            stk.push(arr[i]);
+        } else if (stk[stk.length - 1] === arr[i]) {
+            stk.pop();
+        } else if (stk[stk.length - 1] !== arr[i]) {
+            stk.push(arr[i]);
+        }
+        i++;
+    }
+    return stk.length !== 0 ? stk : [-1];
+}
