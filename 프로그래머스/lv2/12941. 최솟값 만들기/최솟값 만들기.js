29a7f7955a64faf31671824c7c90897c06be9b27
이희령 <winterbooks@naver.com>
2023-09-03 10:13:29 +0900

    [level 2] Title: 최솟값 만들기, Time: 0.66 ms, Memory: 33.1 MB -BaekjoonHub

--- 프로그래머스/lv2/12941. 최솟값 만들기/최솟값 만들기.js
+++ 프로그래머스/lv2/12941. 최솟값 만들기/최솟값 만들기.js
@@ -1,5 +1,5 @@
 function solution(A,B){
-    const sortedA = A.sort((a, b) => a - b);
-    const sortedB = B.sort((a, b) => b - a);
-    return A.reduce((sum, _, i) => sum + (sortedA[i] * sortedB[i]), 0);
+    A.sort((a, b) => a - b);
+    B.sort((a, b) => b - a);
+    return A.reduce((sum, _, i) => sum + (A[i] * B[i]), 0);
 }
