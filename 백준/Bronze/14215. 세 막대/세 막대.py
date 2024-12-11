2accb6c47dfb18ae90884099d253ac470eaec3e3
이희령 <heerealtwo@gmail.com>
2024-12-11 22:48:00 +0900

    [Bronze III] Title: 세 막대, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/14215. 세 막대/세 막대.py
+++ 백준/Bronze/14215. 세 막대/세 막대.py
@@ -1,8 +1,5 @@
-a, b, c = map(int, input().split())
-length_max = max((a, b, c))
-length_sum = sum((a, b, c))
-
-if length_max >= length_sum - length_max:
-    print((length_sum - length_max) * 2 - 1)
+li = sorted(list(map(int, input().split())))
+if li[2] >= li[0] + li[1]:
+    print((li[0] + li[1]) * 2 - 1)
 else:
-    print(length_sum)
+    print(sum(li))
