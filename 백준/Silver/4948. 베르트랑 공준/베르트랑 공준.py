33730f13966748354966b8c19b7178f19c9b449a
이희령 <heerealtwo@gmail.com>
2025-01-19 18:15:26 +0900

    [Silver II] Title: 베르트랑 공준, Time: 1048 ms, Memory: 56048 KB -BaekjoonHub

--- 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.py
+++ 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.py
@@ -1,12 +1,6 @@
-max = 123456*2
-nums = {i : True for i in range(2, max+1)}
+import math
 
-for i in range(2, int(max**0.5)+1):
-    cnt = 2
-    while i*cnt <= max:
-        if nums[i*cnt]:
-            nums[i*cnt] = False
-        cnt += 1
+cases = []
 
 while True:
     num = int(input())
@@ -14,8 +8,28 @@ while True:
     if num == 0:
         break
     else:
-        cnt = 0
-        for i in range(num+1, (num*2)+1):
-            if nums[i]:
-                cnt += 1
+        cases.append(num)
+
+sorted_cases = sorted(cases)
+
+min = sorted_cases[0] + 1
+max = sorted_cases[len(cases) - 1] * 2
+
+nums = {i : True for i in range(min, max + 1)}
+
+for i in range(2, int(max**0.5) + 1):
+    total = i * math.ceil(min/i) * 2
+
+    while total <= max:
+        if nums[total]:
+            nums[total] = False
+        total += i
+
+for num in cases:
+    cnt = 0
+
+    for i in range(num + 1, num*2 + 1):
+        if nums[i]:
+            cnt += 1
+            
     print(cnt)
