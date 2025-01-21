40e74cd9837c3f3b5467ce8351c318e433ad5b33
이희령 <heerealtwo@gmail.com>
2025-01-21 18:15:15 +0900

    [Silver IV] Title: 스택 2, Time: 756 ms, Memory: 88788 KB -BaekjoonHub

--- 백준/Silver/28278. 스택 2/스택 2.py
+++ 백준/Silver/28278. 스택 2/스택 2.py
@@ -11,14 +11,13 @@ for i in range(N):
         stack.append(command[2:])
 
     elif command == '2':
-        print(stack.pop()) if len(stack) > 0 else print(-1)
+        print(stack.pop()) if stack else print(-1)
 
     elif command == '3':
         print(len(stack))
 
     elif command == '4':
-        print(0) if len(stack) > 0 else print(1)
+        print(0) if stack else print(1)
 
     elif command == '5':
-        print(stack[len(stack)-1]) if len(stack) > 0 else print(-1)
-  
+        print(stack[len(stack)-1]) if stack else print(-1)
