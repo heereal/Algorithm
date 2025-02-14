3ec2b4f923e06ea85fd92ca7f952ad9990946de5
이희령 <heerealtwo@gmail.com>
2025-02-14 23:50:07 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.py
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.py
@@ -1,11 +1,9 @@
 N = int(input())
 
-def fibonacci(a, b, cnt):
-    if N == 0:
-        return 0
-    elif cnt >= N:
-        return b
-    else:
-        return fibonacci(b, a+b, cnt+1)
+nums = [0, 1]
 
-print(fibonacci(0, 1, 1))
+for i in range(N-1):
+    num = nums[-1] + nums[-2]
+    nums.append(num)
+
+print(nums[N])
