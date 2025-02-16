e8286d1d5a991444991cb001b48dc534376b4555
이희령 <heerealtwo@gmail.com>
2025-02-16 23:43:38 +0900

    [Bronze II] Title: 저항, Time: 36 ms, Memory: 32544 KB -BaekjoonHub

---
+++ 백준/Bronze/1076. 저항/저항.py
@@ -0,0 +1,7 @@
+colors = ['black', 'brown', 'red', 'orange', 'yellow', 'green', 'blue', 'violet', 'grey', 'white']
+
+first = str(colors.index(input()))
+second = str(colors.index(input()))
+third = 10**colors.index(input())
+
+print(int(first + second) * third)
