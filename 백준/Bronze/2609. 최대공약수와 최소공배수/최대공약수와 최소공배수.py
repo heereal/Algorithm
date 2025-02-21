62c860da59177326803e9a0c19b4e399c4456891
이희령 <heerealtwo@gmail.com>
2025-02-21 16:51:17 +0900

    [Bronze I] Title: 최대공약수와 최소공배수, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.py
+++ 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.py
@@ -1,23 +1,15 @@
 A, B = map(int, input().split())
 
-def gcd(a, b):
+def gcd_func(a, b):
     c = a % b
 
     if c != 0:
-        return gcd(b, c)
+        return gcd_func(b, c)
     else:
         return b
 
+gcd = gcd_func(A, B)
+lcm = (A * B) // gcd
 
-print(gcd(A, B)) # 최대공약수
-
-big = max(A, B)
-small = min(A, B)
-cnt = 1
-
-while True:
-    if (big * cnt) % small == 0:
-        print(big * cnt) # 최대공약수
-        break
-    else:
-        cnt += 1
+print(gcd)
+print(lcm)
