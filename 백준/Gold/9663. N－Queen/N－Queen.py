c906926c16f24cb9619f5719419152f2ccf91eee
이희령 <heerealtwo@gmail.com>
2025-03-08 08:29:49 +0900

    [Gold IV] Title: N-Queen, Time: 28332 ms, Memory: 200788 KB -BaekjoonHub

---
+++ 백준/Gold/9663. N－Queen/N－Queen.py
@@ -0,0 +1,24 @@
+N = int(input())
+
+visited = [-1] * N
+cnt = 0
+
+def check(now_row):
+    for row in range(now_row):
+        if visited[now_row] == visited[row] or now_row - row == abs(visited[now_row] - visited[row]):
+            return False
+    return True
+    
+def dfs(row):
+    if row == N:
+        global cnt
+        cnt += 1
+        return
+
+    for col in range(N):
+        visited[row] = col
+        if check(row):
+            dfs(row + 1)
+
+dfs(0)
+print(cnt)
