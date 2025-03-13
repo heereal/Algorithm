23c85bf311eb4b22694d04717b0d9d6efd18c536
이희령 <heerealtwo@gmail.com>
2025-03-13 18:04:17 +0900

    [Gold V] Title: 두 용액, Time: 116 ms, Memory: 44748 KB -BaekjoonHub

---
+++ 백준/Gold/2470. 두 용액/두 용액.py
@@ -0,0 +1,20 @@
+N = int(input())
+nums = sorted(list(map(int, input().split())))
+
+left, right = 0, N-1
+min = abs(nums[left] + nums[right])
+result = [nums[left], nums[right]]
+
+while left < right:
+    sum = nums[left] + nums[right]
+    
+    if abs(sum) < min:
+        min = abs(sum)
+        result = [nums[left], nums[right]]
+        
+    if sum > 0:
+        right -= 1
+    else:
+        left += 1
+
+print(result[0], result[1])
