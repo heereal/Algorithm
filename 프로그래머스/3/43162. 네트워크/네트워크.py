cdc83d9b04e882517982243125778546af8b5e04
이희령 <heerealtwo@gmail.com>
2025-03-20 14:15:59 +0900

    [level 3] Title: 네트워크, Time: 2.30 ms, Memory: 9.31 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.py
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -1,17 +1,23 @@
+from collections import deque
+
 def solution(n, computers):
-    def dfs(i):
-        visited[i] = True
+    def bfs(i):
+        queue = deque([i])
         
-        for j in range(n):
-            if not visited[j] and computers[j][i]:
-                dfs(j)
-    
+        while queue:
+            node = queue.popleft()
+            visited[node] = True
+            
+            for j in range(n):
+                if not visited[j] and computers[j][node]:
+                    queue.append(j)
+            
     visited = [False] * n
     cnt = 0
             
     for i in range(n):
         if not visited[i]:
+            bfs(i)
             cnt += 1
-            dfs(i)
         
     return cnt
