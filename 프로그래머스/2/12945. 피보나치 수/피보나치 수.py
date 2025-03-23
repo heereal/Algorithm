5b991cb4e346d5b89e621a82ce09e0cff9ee3d97
이희령 <heerealtwo@gmail.com>
2025-03-24 08:33:14 +0900

    [level 2] Title: 피보나치 수, Time: 383.99 ms, Memory: 455 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.py
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.py
@@ -1,7 +1,7 @@
 def solution(n):
-    before, now = 0, 1
+    memo = [0, 1] + [0] * (n-1)
     
-    for i in range(2, n):
-        before, now = now, before + now
+    for i in range(2, n+1):
+        memo[i] = memo[i-1] + memo[i-2]
         
-    return (before + now) % 1234567
+    return memo[n] % 1234567
