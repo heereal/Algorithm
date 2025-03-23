7b73b0f70cb0be045c511684e67f04527b0e88e1
이희령 <heerealtwo@gmail.com>
2025-03-23 14:07:55 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.01 ms, Memory: 9.02 MB -BaekjoonHub

--- 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.py
+++ 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.py
@@ -1,14 +1,11 @@
-def count_one(n):
-    # n을 2진수로 변환 후 1의 개수 카운트
-    count = list(bin(n)[2:]).count("1")
-    return count
-
 def solution(n):
-    now = count_one(n)
-    next = 0
+    now = bin(n).count("1")
     
-    while now != next:
+    while True:
         n += 1
-        next = count_one(n)
+        next = bin(n).count("1")
+        
+        if now == next:
+            break
         
     return n
