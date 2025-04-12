7ab1bd95da8438094a27c8cb40dca004774ad4b5
이희령 <heerealtwo@gmail.com>
2025-04-12 15:06:31 +0900

    [Silver III] Title: N번째 큰 수, Time: 808 ms, Memory: 35508 KB -BaekjoonHub

--- 백준/Silver/2075. N번째 큰 수/N번째 큰 수.py
+++ 백준/Silver/2075. N번째 큰 수/N번째 큰 수.py
@@ -7,11 +7,12 @@ heap = []
 
 for _ in range(N):
     row = list(map(int, input().split()))
-    
+
     for num in row:
         if len(heap) < N:
             heapq.heappush(heap, num)
         else:
-            heapq.heappushpop(heap, num)
+            if heap[0] < num:
+                heapq.heappushpop(heap, num)
 
 print(heap[0])
