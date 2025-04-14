053f0b5c6de5a92ac88ab0cd8caa00711f0ceefd
이희령 <heerealtwo@gmail.com>
2025-04-14 15:27:11 +0900

    [Gold II] Title: 트리의 지름, Time: 552 ms, Memory: 88856 KB -BaekjoonHub

--- 백준/Gold/1167. 트리의 지름/트리의 지름.py
+++ 백준/Gold/1167. 트리의 지름/트리의 지름.py
@@ -1,6 +1,6 @@
 import sys
-from collections import deque
 input = sys.stdin.readline
+sys.setrecursionlimit(10 ** 9)
 
 V = int(input())
 tree = [[] for _ in range(V+1)]
@@ -12,24 +12,21 @@ for _ in range(V):
     for i in range(1, len(info)-1, 2):
         tree[node].append((info[i], info[i+1]))
 
-def BFS(start):
-    visited = [-1] * (V+1)
-    visited[start] = 0
-    queue = deque([start])
+def DFS(node, distance):
+    for next_node, next_distance in tree[node]:
+        if visited[next_node] == -1:
+            visited[next_node] = visited[node] + next_distance
+            DFS(next_node, distance + next_distance)
 
-    while queue:
-        now = queue.popleft()
+visited = [-1] * (V+1)
+visited[1] = 0
+DFS(1, 0)
 
-        for next_node, next_distance in tree[now]:
-            if visited[next_node] == -1:
-                queue.append(next_node)
-                visited[next_node] = visited[now] + next_distance
+farthest_distance = max(visited)
+farthest_node = visited.index(farthest_distance)
 
-    farthest_distance = max(visited)
-    farthest_node = visited.index(farthest_distance)
-    return farthest_node, visited
-
-farthest_node, _ = BFS(1)
-_, visited = BFS(farthest_node)
+visited = [-1] * (V+1)
+visited[farthest_node] = 0
+DFS(farthest_node, 0)
 
 print(max(visited))
