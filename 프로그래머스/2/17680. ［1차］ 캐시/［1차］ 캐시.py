28857091f28e420adfc95903b1d7c6068a947542
이희령 <heerealtwo@gmail.com>
2025-05-16 10:40:20 +0900

    [level 2] Title: [1차] 캐시, Time: 71.70 ms, Memory: 16.6 MB -BaekjoonHub

--- 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.py
+++ 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.py
@@ -1,30 +1,18 @@
 from collections import deque
 
 def solution(cacheSize, cities):
-    cache = deque()
-    answer = 0
+    cache = deque(maxlen=cacheSize)
+    time = 0
     
-    if cacheSize == 0:
-        return 5 * len(cities) 
-    
-    for i in range(len(cities)):
-        city = cities[i].lower()
+    for city in cities:
+        city = city.lower()
         
-        if len(cache) < cacheSize:
-            if city not in cache:
-                answer += 5
-                cache.append(city)
-            else:
-                answer += 1
-                cache.remove(city)
-                cache.append(city)
-        elif city in cache:
-            answer += 1
+        if city in cache:
+            time += 1
             cache.remove(city)
             cache.append(city)
         else:
-            answer += 5
-            cache.popleft()
+            time += 5
             cache.append(city)
     
-    return answer
+    return time
