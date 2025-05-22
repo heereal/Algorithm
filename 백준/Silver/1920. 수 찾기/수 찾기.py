db9dd3c32808d9a0dc037fe5fac88396eb8b0e4d
이희령 <heerealtwo@gmail.com>
2025-05-22 13:03:27 +0900

    [Silver IV] Title: 수 찾기, Time: 512 ms, Memory: 48616 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.py
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -6,19 +6,19 @@ ms = list(map(int, input().split()))
 ns.sort()
 
 def binary_search(low, high, target):
-    if low > high:
-        return 0
-
-    mid = (low + high) // 2
-
-    if ns[mid] == target:
-        return 1
-    elif ns[mid] > target:
-        high = mid-1
-        return binary_search(low, high, target)
-    elif ns[mid] < target:
-        low = mid+1
-        return binary_search(low, high, target)
+    while low <= high:
+        mid = (low + high) // 2
+        
+        if ns[mid] == target:
+            return 1
+        elif ns[mid] > target:
+            high = mid - 1
+            return binary_search(low, high, target)
+        elif ns[mid] < target:
+            low = mid + 1
+            return binary_search(low, high, target)
+    
+    return 0
 
 for num in ms:
     print(binary_search(0, n-1, num))
