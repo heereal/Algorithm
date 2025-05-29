884492fe8ba6b44d81e5ba327279e5e96535ab6d
이희령 <heerealtwo@gmail.com>
2025-05-29 17:05:29 +0900

    [level 0] Title: 전국 대회 선발 고사, Time: 0.03 ms, Memory: 9.1 MB -BaekjoonHub

--- 프로그래머스/0/181851. 전국 대회 선발 고사/전국 대회 선발 고사.py
+++ 프로그래머스/0/181851. 전국 대회 선발 고사/전국 대회 선발 고사.py
@@ -1,14 +1,9 @@
 def solution(rank, attendance):
     answer = []
-    cnt = 0
     
-    for i, attend in zip(sorted(rank), attendance):
-        j = rank.index(i)
-        if attendance[j]:
-            answer.append(j)
-            cnt += 1
-        if cnt == 3:
-            break
+    for i, attend in enumerate(attendance):
+        if attend:
+            answer.append((rank[i], i))
     
-    a, b, c = answer
-    return (a * 10000) + (b * 100) + c
+    a, b, c = sorted(answer)[:3]
+    return (a[1] * 10000) + (b[1] * 100) + c[1]
