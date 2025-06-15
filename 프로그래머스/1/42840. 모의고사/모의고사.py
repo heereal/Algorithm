cc9417b5e0e0bb3f7e678f172219f6a1db9a2b10
이희령 <heerealtwo@gmail.com>
2025-06-15 13:09:46 +0900

    [level 1] Title: 모의고사, Time: 4.24 ms, Memory: 9.31 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.py
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -3,19 +3,19 @@ def solution(answers):
     second = [2, 1, 2, 3, 2, 4, 2, 5]
     third = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
     
-    cnt = [0, 0, 0, 0]
+    score = [0, 0, 0]
     answer = []
     
     for i in range(len(answers)):
         if answers[i] == first[i % len(first)]:
-            cnt[1] += 1
+            score[0] += 1
         if answers[i] == second[i % len(second)]:
-            cnt[2] += 1
+            score[1] += 1
         if answers[i] == third[i % len(third)]:
-            cnt[3] += 1
+            score[2] += 1
             
-    for i in range(1, 4):
-        if cnt[i] >= max(cnt):
-            answer.append(i)
+    for i, s in enumerate(score):
+        if s >= max(score):
+            answer.append(i+1)
             
     return answer
