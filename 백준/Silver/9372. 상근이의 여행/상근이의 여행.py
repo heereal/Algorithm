7760410439b3128e324023ea158437b328805ed9
이희령 <heerealtwo@gmail.com>
2025-06-16 11:29:50 +0900

    [Silver IV] Title: 상근이의 여행, Time: 56 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/9372. 상근이의 여행/상근이의 여행.py
+++ 백준/Silver/9372. 상근이의 여행/상근이의 여행.py
@@ -1,23 +1,12 @@
 import sys
 input = sys.stdin.readline
 
-def DFS(node, cnt):
-    visited[node] = True
-    for n in graph[node]:
-        if not visited[n]:
-            cnt = DFS(n, cnt + 1)
-    return cnt
-
 T = int(input())
 
 for _ in range(T):
     N, M = map(int, input().split())
-    graph = [[] for _ in range(N + 1)]
 
     for _ in range(M):
-        A, B = map(int, input().split())
-        graph[A].append(B)
-        graph[B].append(A)
+        input()
 
-    visited = [False] * (N + 1)
-    print(DFS(1, 0))
+    print(N - 1)
