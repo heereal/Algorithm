823c4c76279811d47a0847fa697a0457fdb4a467
이희령 <heerealtwo@gmail.com>
2025-06-17 08:02:21 +0900

    [level 1] Title: 과일 장수, Time: 71.12 ms, Memory: 20.8 MB -BaekjoonHub

--- 프로그래머스/1/135808. 과일 장수/과일 장수.py
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.py
@@ -1,9 +1,4 @@
 def solution(k, m, score):
-    answer = 0
-    score.sort(reverse=True)
-    
-    for i in range(len(score) // m):
-        box = score[i*m:(i*m + m)]
-        answer += min(box) * m
-        
-    return answer
+    score.sort()
+    min_list = score[len(score) % m::m]   
+    return sum(min_list) * m
