479dd62be9979024f901eb5dee9606c3cc3b207d
이희령 <heerealtwo@gmail.com>
2025-06-19 08:56:24 +0900

    [level 1] Title: 소수 찾기, Time: 341.89 ms, Memory: 114 MB -BaekjoonHub

--- 프로그래머스/1/12921. 소수 찾기/소수 찾기.py
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.py
@@ -1,12 +1,8 @@
 def solution(n):
-    nums = [False, False] + [True] * (n - 1)
-    primes = []
+    nums = set(range(2, n +1))
     
     for i in range(2, n + 1):
-        if nums[i]:
-            primes.append(i)
+        if i in nums:
+            nums -= set(range(2 * i, n + 1, i))
             
-            for j in range(2 * i, n + 1, i):
-                nums[j] = False
-            
-    return len(primes)
+    return len(nums)
