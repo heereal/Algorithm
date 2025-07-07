e86cea75aa0fa85be545d3a1e0cc07b05947b15c
이희령 <heerealtwo@gmail.com>
2025-07-07 09:12:53 +0900

    [level 2] Title: 스킬트리, Time: 0.02 ms, Memory: 9.08 MB -BaekjoonHub

--- 프로그래머스/2/49993. 스킬트리/스킬트리.py
+++ 프로그래머스/2/49993. 스킬트리/스킬트리.py
@@ -4,18 +4,15 @@ def solution(skill, skill_trees):
     
     for tree in skill_trees:
         check = [1] * len(skill)
-        is_possible = True
         
         for s in tree:
             if s in skill:
                 i = skill.index(s)
                 if sum(check[:i]) >= 1:
-                    is_possible = False
                     break
                 else:
                     check[i] = 0
-
-        if is_possible:
+        else:
             answer += 1
             
     return answer
