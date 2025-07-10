4b43b8a0a61d4f63f0884d9034ef703f43b8f2e1
이희령 <heerealtwo@gmail.com>
2025-07-11 08:37:49 +0900

    [level 2] Title: [3차] 압축, Time: 4.24 ms, Memory: 9.23 MB -BaekjoonHub

--- 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.py
+++ 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.py
@@ -1,5 +1,5 @@
 def solution(msg):
-    words = [0, "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+    words = list('_ABCDEFGHIJKLMNOPQRSTUVWXYZ')
     answer = []
     
     while msg:
