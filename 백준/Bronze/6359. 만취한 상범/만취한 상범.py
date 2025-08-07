e54f7b7b6cad8a712b5829b5a697d2a57d4cb257
이희령 <heerealtwo@gmail.com>
2025-08-08 08:10:32 +0900

    [Bronze II] Title: 만취한 상범, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/6359. 만취한 상범/만취한 상범.py
+++ 백준/Bronze/6359. 만취한 상범/만취한 상범.py
@@ -5,12 +5,10 @@ for _ in range(t):
     arr = [1] * n
 
     for i in range(2, n+1):
-        j = i
-        while j <= n:
+        for j in range(i, n+1, i):
             if arr[j-1] == 1:
                 arr[j-1] = 0
             else:
                 arr[j-1] = 1
-            j += i
     
     print(sum(arr))
