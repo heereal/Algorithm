6721badae24f1120b74c0b617ab3c96640e2e5b5
이희령 <heerealtwo@gmail.com>
2025-08-20 08:04:32 +0900

    [Bronze I] Title: 배스킨라빈스 31, Time: 56 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/14429. 배스킨라빈스 31/배스킨라빈스 31.py
+++ 백준/Bronze/14429. 배스킨라빈스 31/배스킨라빈스 31.py
@@ -4,11 +4,7 @@ res = []
 for _ in range(n):
     j, m = map(int, input().split())
     win_num = (j - 1) % (m + 1)
-    cnt = 0
-    
-    for _ in range(win_num, j + 1, m + 1):
-        cnt += 1
-        
+    cnt = (j - win_num) // (m + 1) + 1
     res.append(cnt)
 
 min_turn = min(res)
