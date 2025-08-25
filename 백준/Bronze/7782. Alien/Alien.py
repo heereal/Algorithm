f4dd5ee5971cd43f1222b0249271c31adc04f2d9
이희령 <heerealtwo@gmail.com>
2025-08-25 10:05:01 +0900

    [Bronze III] Title: Alien, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/7782. Alien/Alien.py
+++ 백준/Bronze/7782. Alien/Alien.py
@@ -1,3 +1,6 @@
+import sys
+input = sys.stdin.readline
+
 n = int(input())
 b1, b2 = map(int, input().split())
 answer = False
@@ -6,5 +9,8 @@ for _ in range(n):
     lx, ly, hx, hy = map(int, input().split())
     if lx <= b1 <= hx and ly <= b2 <= hy:
         answer = True
+        print('Yes')
+        break
+else:
+    print('No')
 
-print('Yes' if answer else 'No')
