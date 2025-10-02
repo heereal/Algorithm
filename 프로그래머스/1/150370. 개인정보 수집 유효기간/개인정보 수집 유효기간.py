8be0d4dee0afcf8d20327d05d6353a84e48c8831
이희령 <heerealtwo@gmail.com>
2025-10-02 09:05:06 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 0.22 ms, Memory: 9.36 MB -BaekjoonHub

--- 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.py
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.py
@@ -1,24 +1,20 @@
 def solution(today, terms, privacies):
     dic = {}
-    dates = []
+    answer = []
     
     for term in terms:
         type, month = term.split()
         dic[type] = int(month)
+        
+    y, m, d = map(int, today.split('.'))
+    t_days = (y * 28 * 12) + (m * 28) + d
     
     for i, p in enumerate(privacies):
         date, type = p.split()
         y, m, d = map(int, date.split('.'))
-        
         days = (y * 28 * 12) + ((m + dic[type]) * 28) + d
-        dates.append((days, i + 1))
-    
-    y, m, d = map(int, today.split('.'))
-    t_days = (y * 28 * 12) + (m * 28) + d
-    answer = []
-
-    for days, i in dates:
+        
         if t_days >= days:
-            answer.append(i)
+            answer.append(i + 1)
 
     return answer
