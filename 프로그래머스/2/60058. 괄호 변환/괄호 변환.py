1850053f57862cd0160397afd14cf9f2b1d7d7d1
이희령 <heerealtwo@gmail.com>
2025-11-12 09:44:07 +0900

    [level 2] Title: 괄호 변환, Time: 1.22 ms, Memory: 9.58 MB -BaekjoonHub

---
+++ 프로그래머스/2/60058. 괄호 변환/괄호 변환.py
@@ -0,0 +1,54 @@
+from collections import deque
+
+def reverse_str(u):
+    res = ""
+    for s in u:
+        if s == "(":
+            res += ")"
+        else:
+            res += "("
+
+    return res
+
+def is_valid(u):
+    stack = []
+    
+    for s in u:
+        if s == '(':
+            stack.append(s)
+        elif not stack:
+            return False
+        else:
+            stack.pop()
+        
+    if stack:
+        return False
+    else:
+        return True
+
+def solution(p):
+    queue = deque(p)
+    if not queue:
+        return ""
+    
+    s, e = 0, 0
+    u = ""
+
+    while queue:
+        cur = queue.popleft()
+        u += cur
+        
+        if cur == "(":
+            s += 1
+        else:
+            e += 1
+            
+        if s > 0 and e > 0 and s == e:
+            break
+    
+    v = solution("".join(queue))
+   
+    if is_valid(u):
+        return u + v
+    else:
+        return "(" + v + ")" + reverse_str(u[1:-1])
