a0a7707b3d6fbf057c1ed8058f002caffed76245
이희령 <heerealtwo@gmail.com>
2025-11-17 08:27:58 +0900

    [level 0] Title: 안전지대, Time: 0.07 ms, Memory: 9.24 MB -BaekjoonHub

--- 프로그래머스/0/120866. 안전지대/안전지대.py
+++ 프로그래머스/0/120866. 안전지대/안전지대.py
@@ -1,17 +1,10 @@
 def solution(board):
     m, n = len(board), len(board[0])
-    moves = [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
-    answer = 0
+    danger = set()
     
     for i in range(m):
         for j in range(n):
             if board[i][j] == 1:
-                for dx, dy in moves:
-                    nx, ny = i + dx, j + dy
-                    if 0 <= nx < m and 0 <= ny < n and board[nx][ny] != 1:
-                        board[nx][ny] = -1
-    
-    for line in board:
-        answer += line.count(0)
+                danger.update((i + dx, j + dy) for dx in [-1, 0, 1] for dy in [-1, 0, 1])
                 
-    return answer
+    return n * n - sum(0 <= x < m and 0 <= y < n for x, y in danger)
