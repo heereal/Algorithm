a49fa3074b8b11125d153d5f49d8660edff8b22a
이희령 <heerealtwo@gmail.com>
2025-11-22 10:10:41 +0900

    [level 1] Title: 공원 산책, Time: 0.11 ms, Memory: 9.46 MB -BaekjoonHub

--- 프로그래머스/1/172928. 공원 산책/공원 산책.py
+++ 프로그래머스/1/172928. 공원 산책/공원 산책.py
@@ -1,28 +1,28 @@
+def check_is_possible(x, y, dx, dy, park):
+    is_dx_movable = True
+
+    if y != dy:
+        is_dx_movable = False
+
+    if is_dx_movable:
+        for i in range(x, dx + 1):
+            if park[i][y] == 'X':
+                return False
+    else:
+        for i in range(y, dy + 1):
+            if park[x][i] == 'X':
+                return False    
+
+    return True
+
 def solution(park, routes):
     m, n = len(park), len(park[0])
-    
     cur = [0, 0]
+    
     for i in range(n):
         if 'S' in park[i]:
             cur = [i, park[i].index('S')]
             break
-    
-    def check_is_possible(x, y, dx, dy):
-        is_dx_movable = True
-        
-        if y != dy:
-            is_dx_movable = False
-       
-        if is_dx_movable:
-            for i in range(x, dx + 1):
-                if park[i][y] == 'X':
-                    return False
-        else:
-            for i in range(y, dy + 1):
-                if park[x][i] == 'X':
-                    return False    
-
-        return True
 
     for s in routes:
         op, d = s.split()
@@ -39,7 +39,7 @@ def solution(park, routes):
             nx, ny = x, y + d
         
         if 0 <= nx < m and 0 <= ny < n:
-            if check_is_possible(min(x, nx), min(y, ny), max(x, nx), max(y, ny)):
+            if check_is_possible(min(x, nx), min(y, ny), max(x, nx), max(y, ny), park):
                 cur = [nx, ny]
      
     return cur
