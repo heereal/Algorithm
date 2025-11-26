0f66c994e8552208a14d0570c03927f920ef3a5d
이희령 <heerealtwo@gmail.com>
2025-11-27 07:57:16 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 1.26 ms, Memory: 9.32 MB -BaekjoonHub

--- 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.py
+++ 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.py
@@ -1,26 +1,19 @@
 def solution(board, moves):
-    n = len(board)
+    stack = []
     answer = 0
-    
-    stack = [[] for _ in range(n + 1)]
-    basket = []
-    board.reverse()
-    
-    for i in range(n):
-        for j in range(n):
-            if board[j][i]:
-                stack[i + 1].append(board[j][i])
         
     for i in moves:
-        if stack[i]:
-            next = stack[i].pop()
-            
-            if not basket:
-                basket.append(next)
-            elif basket[-1] == next:
-                answer += 2
-                basket.pop()
-            else:
-                basket.append(next)
-        
+        for j in range(len(board)):
+            if board[j][i-1]:
+                stack.append(board[j][i-1])
+                board[j][i-1] = 0
+                
+                if len(stack) > 1:
+                    if stack[-1] == stack[-2]:
+                        stack.pop()
+                        stack.pop()
+                        answer += 2
+
+                break
+
     return answer
