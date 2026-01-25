868689b463abd0f3777842deec5856a6fa84212d
이희령 <heerealtwo@gmail.com>
2026-01-25 19:00:47 +0900

    [Bronze II] Title: 나의 학점은?, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/17826. 나의 학점은？/나의 학점은？.py
@@ -0,0 +1,18 @@
+scores = list(map(int, input().split()))
+mine = int(input())
+rank = scores.index(mine) + 1
+
+if rank <= 5:
+    print('A+')
+elif rank <= 15:
+    print('A0')
+elif rank <= 30:
+    print('B+')
+elif rank <= 35:
+    print('B0')
+elif rank <= 45:
+    print('C+')
+elif rank <= 48:
+    print('C0')
+else:
+    print('F')
