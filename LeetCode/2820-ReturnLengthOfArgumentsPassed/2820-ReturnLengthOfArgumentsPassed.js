b31d7b3c1b7fda680daec83ed8db1c0ad0082b34
이희령 <heerealtwo@gmail.com>
2026-04-18 19:25:30 +0900

    Create 2820-ReturnLengthOfArgumentsPassed

---
+++ LeetCode/2820-ReturnLengthOfArgumentsPassed/2820-ReturnLengthOfArgumentsPassed.js
@@ -0,0 +1,13 @@
+// Last updated: 2026. 4. 18. 오후 7:25:30
+/**
+ * @param {...(null|boolean|number|string|Array|Object)} args
+ * @return {number}
+ */
+var argumentsLength = function(...args) {
+    return args.length
+    
+};
+
+/**
+ * argumentsLength(1, 2, 3); // 3
+ */
